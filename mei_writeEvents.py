ed1c2f4f82263469bf7bc526b9790ed2cd43a719
Anna Plaksin <annplaksin@gmx.net>
2024-09-17 14:33:51 +0200

    Start file for events

---
+++ mei_writeEvents.py
@@ -0,0 +1,43 @@
+
+# ----------------------- #
+# event-related functions #
+# ----------------------- #
+
+
+def makeNote():
+	# create <note> from <Note>
+	# then converPitch()
+	# convertDuration()
+	# convertModernTexttoLyrics()
+	return None
+
+def convertPitch():
+	# <LetterName>D</LetterName> -> @pname
+    # <OctaveNum>3</OctaveNum> -> @oct
+	# octave count differs in cmme, octave starts at a
+	# a(4) & b(4) are identical in cmme and mei
+	# if range(c:g) => oct = oct + 1
+
+	return None
+	
+def convertDuration():
+
+	# <Type> value="Semifusa|Fusa|Semiminima|Minima|Semibrevis|Brevis|Longa|Maxima"	# <Type> to @dur de-capitalize first letter, like Maxima -> maxima
+	# <Length> containing <Num>/<Den> use 1/1 for	minima
+	# everything smaller than minima is always binary, e.g. 1/2 for semiminima
+	# cmme stores relative durations always in every note/rest, one can figure out mensuration from that
+	# -> Convert <Num> to @num && <Den> to @numbase
+	return None 
+
+
+def convertModernTexttoLyrics():
+	# <ModernText><Syllable></ModernText> (child of <Note>) to <verse><syl></verse>
+	# <WordEnd> to @wordpos="t" (cmme doesn't distinguish i & m)
+    # !<WordEnd> -
+	# 	@con="d"d"
+    # remember if last <ModernText> had <Wordend>
+	#		if lastText.wordend == True
+	#			@wordpos="i"
+    #       else
+    #           @wordpos="m"
+	return None
